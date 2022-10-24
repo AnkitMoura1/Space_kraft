4daa0aa5ff3a3800ffa3cb53821ac8a39715e459
AnkitMoura1 <ankit1moura@gmail.com>
2022-10-24 11:40:21 +0530

    movement added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+
 WIDTH, HEIGHT = 900, 500
 SHIP_H, SHIP_W = 55, 40
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -10,16 +11,41 @@ red_ship = pygame.image.load('Resources/redship.png')
 red_ship = pygame.transform.scale(red_ship, (SHIP_W, SHIP_H))
 blue_ship = pygame.image.load('Resources/blueship.png')
 blue_ship = pygame.transform.scale(blue_ship, (SHIP_W, SHIP_H))
+SPEED = 1
 
 
-def draw_window():
+def draw_window(red, blue):
     WIN.fill(WHITE)
-    WIN.blit(red_ship, (250, 200))
-    WIN.blit(blue_ship, (650, 200))
+    WIN.blit(red_ship, (red.x, red.y))
+    WIN.blit(blue_ship, (blue.x, blue.y))
     pygame.display.update()
 
 
+def red_movement(red, keys_pressed):
+    if keys_pressed[pygame.K_a]:  # red left
+        red.x -= 1 + SPEED
+    if keys_pressed[pygame.K_d]:  # red right
+        red.x += 1 + SPEED
+    if keys_pressed[pygame.K_w]:  # red up
+        red.y -= 1 + SPEED
+    if keys_pressed[pygame.K_s]:  # red down
+        red.y += 1 + SPEED
+
+
+def blue_movement(blue, keys_pressed):
+    if keys_pressed[pygame.K_LEFT]:  # blue left
+        blue.x -= 1 + SPEED
+    if keys_pressed[pygame.K_RIGHT]:  # blue right
+        blue.x += 1 + SPEED
+    if keys_pressed[pygame.K_UP]:  # blue up
+        blue.y -= 1 + SPEED
+    if keys_pressed[pygame.K_DOWN]:  # blue down
+        blue.y += 1 + SPEED
+
+
 def main():
+    red = pygame.Rect([250, 200, SHIP_W, SHIP_H])
+    blue = pygame.Rect([650, 200, SHIP_W, SHIP_H])
     run = True
     clock = pygame.time.Clock()
     while run:
@@ -27,7 +53,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        draw_window(red, blue)
+        keys_pressed = pygame.key.get_pressed()
+        red_movement(red, keys_pressed)
+        blue_movement(blue, keys_pressed)
     pygame.quit()
 
 
